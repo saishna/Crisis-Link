96eb84aae9afa142d93a2dd0d39a9783afc51b70
Pragati Shrestha <pragati.shrestha07@gmail.com>
2025-04-25 12:07:36 +0545

    descriptionadded

--- src/sections/FloodZoneManagement.jsx
+++ src/sections/FloodZoneManagement.jsx
@@ -84,6 +84,8 @@ const FloodZoneManagement = () => {
           coordinates: ["", ""],
           address: "",
         },
+        description:"",//changed
+        
         riskLevel: "Low",
         resolved: false,
         inReview: false,
@@ -348,6 +350,22 @@ const FloodZoneManagement = () => {
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
                 </div>
+//changed
+                <div>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    value={newZone.description}
+                    onChange={(e) =>
+                      setNewZone({ ...newZone, description: e.target.value })
+                    }
+                    
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+//,,,,,
                 <div>
                   <label className="block text-sm font-medium text-gray-700 mb-2">
                     Risk Level
